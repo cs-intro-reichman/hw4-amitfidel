ac804d6821268a8d17369ed7462acc0b38ee980f
amitfidel <amitfidel22@gmail.com>
2024-11-30 19:26:45 +0200

    test11

--- Primes.java
+++ Primes.java
@@ -1,36 +1,27 @@
 public class Primes {
-    public static void main(int n) 
+    public static void main(String[] args) 
     {
-        String[] args=new String[n+1];
-        for(int i=0;i<n+1;i++)
-        {
-            args[i]=String.valueOf(i);
-        }
-        //String[] args2=new String[7];
-        if(args.length<2)
+        int n=Integer.parseInt(args[0]);
+        if(n<2)
         {
         System.out.println("no primes here");
         return;
         }
-        else
-        {
-        int[] numbers=new int[args.length];
-        boolean[] isPrime=new boolean[numbers.length];
+        boolean[] isPrime=new boolean[n+1];
         int counter=0;
         for (int i=0;i<isPrime.length;i++)
         {
-            numbers[i]=Integer.parseInt(args[i]);
             isPrime[i]=true;
         }
         isPrime[0]=false;
         isPrime[1]=false;
-        for(int i=2;i<isPrime.length;i++)
+        for(int i=2;i<Math.sqrt(n);i++)
         {
             if (isPrime[i]==true) 
             {
-                for (int j=i+1;j<numbers.length;j++)
+                for (int j=i+1;j<isPrime.length;j++)
                 {
-                    if (numbers[j]%numbers[i]==0)
+                    if (j%i==0)
                     {
                         isPrime[j]=false; 
                     }
@@ -41,13 +32,12 @@ public class Primes {
         {
             if(isPrime[i])
             {
-                System.out.println(numbers[i]);
+                System.out.println(i);
                 counter++;
             }
         }
-        int num=numbers.length-1;
+        int num=n;
         int precent=(int)(((double)counter/(double)(num))*100);
         System.out.println("There are "+counter+" primes between 2 and "+num+" ("+precent+"% are primes)");
-    }
     }
 }
