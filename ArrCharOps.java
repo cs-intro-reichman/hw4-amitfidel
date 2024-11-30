73a099ed6cfff0bfbb18c76988fdd663ee66af45
amitfidel <amitfidel22@gmail.com>
2024-11-30 20:17:35 +0200

    test18

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -152,14 +152,13 @@ public class ArrCharOps {
     public static long hashCode(char[] arr) {
         long l=0;
         int n=arr.length;
-        if (n<1) 
+        if (arr.length<1) 
         {
             return 0;    
         }
         for(int i=0;i<arr.length;i++)
         {
             l+=arr[i]*7^(n-(i+1));
-            n--;
         }
         return l;
     }
